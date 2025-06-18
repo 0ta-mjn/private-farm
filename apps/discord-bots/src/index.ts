d88f90918d9828a8e6ce6a98b2e32fdbac6c8078
mininjin <mininjin332@gmail.com>
2025-06-18 16:59:28 +0900

    feat: initialize discord-bots app with basic structure and configurations

---
+++ apps/discord-bots/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { handler as helloWorld } from "./hello-world";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+
+const endpoints = {
+  helloWorld,
+};
+
+Object.entries(endpoints).forEach(([path, handler]) => {
+  app.post(`/${path}`, handler);
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  const url = `http://localhost:${PORT}`;
+  console.log(`Server is running at ${url}`);
+  console.log(
+    `Available endpoints:\n${Object.keys(endpoints)
+      .map((key) => `- POST ${url}/${key}`)
+      .join("\n")}`
+  );
+});
