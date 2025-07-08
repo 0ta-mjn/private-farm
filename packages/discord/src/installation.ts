719e8a3d5c413cb4efc4b1c09baca28bae339dd6
mininjin <mininjin332@gmail.com>
2025-07-08 09:22:08 +0900

    fix: specify type for error response in registerDiscordChannel function

--- packages/discord/src/installation.ts
+++ packages/discord/src/installation.ts
@@ -57,7 +57,7 @@ export async function registerDiscordChannel(
   });
 
   if (!tokRes.ok) {
-    const res: any = await tokRes.json();
+    const res: { code?: string } = await tokRes.json();
     throw createDiscordErrorFromResponse(res, tokRes.status);
   }
 
