4bd160a83942cc61f52a2d733d77aecbd28d9ebe
mininjin <mininjin332@gmail.com>
2025-07-11 19:03:22 +0900

    feat: sort device info by time

--- apps/sensor-ingest/src/consumer/handler.ts
+++ apps/sensor-ingest/src/consumer/handler.ts
@@ -1,9 +1,10 @@
 import { DeviceInfoInput, SensorDB } from "@repo/sensor-db";
 import { parseMessage } from "./parser/chirpstack";
+import { EnQueuedMessage } from "../interfaces";
 
 export const bulkInsertSensorData = async (
   db: SensorDB,
-  messages: { event: string; data: unknown }[]
+  messages: EnQueuedMessage[]
 ) => {
   const parsedMessages = messages.map((msg) => ({
     event: msg.event,
@@ -14,8 +15,7 @@ export const bulkInsertSensorData = async (
     .map(({ data }) => {
       if (!data) return [];
 
-      if (data.values.length === 0) {
-        console.warn("No valid sensor values found in message:", data);
+      if (!data.values || data.values.length === 0) {
         return [];
       }
 
@@ -32,28 +32,29 @@ export const bulkInsertSensorData = async (
   // Perform bulk insert using the ingest repository
   await db.ingest.bulkInsert(sensorDataArray);
 
-  const devices = parsedMessages.reduce<Map<string, DeviceInfoInput>>(
-    (map, { data }) => {
-      if (!data || !data.deviceInfo) return map;
-
+  // ソートして最新のデバイス情報のみを保持
+  const devices = parsedMessages
+    .filter(({ data }) => data && data.deviceInfo?.devEui)
+    .sort((a, b) => {
+      const timeA = a.data?.time ? new Date(a.data.time).getTime() : 0;
+      const timeB = b.data?.time ? new Date(b.data.time).getTime() : 0;
+      return timeB - timeA; // 新しい順
+    })
+    .reduce<Map<string, DeviceInfoInput>>((map, { data }) => {
+      if (!data) return map;
       const { devEui, deviceName, applicationId, applicationName } =
         data.deviceInfo;
-      if (!devEui) {
-        console.warn("Device EUI is missing in message:", data);
-        return map;
+      if (!devEui) return map;
+      if (!map.has(devEui)) {
+        map.set(devEui, {
+          devEui,
+          name: deviceName || "",
+          applicationId,
+          applicationName,
+        });
       }
-
-      // Create or update device info
-      map.set(devEui, {
-        devEui,
-        name: deviceName || "",
-        applicationId: applicationId,
-        applicationName: applicationName,
-      });
-
       return map;
-    },
-    new Map()
-  );
+    }, new Map());
+
   await db.ingest.bulkUpsertDeviceInfo(Array.from(devices.values()));
 };
